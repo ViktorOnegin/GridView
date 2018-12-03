2946b29348265622abbf77f23a01e8e0be14d83c
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2018-12-03 09:15:19 +0200

    xd

---
+++ HelloGridView/HelloGridView/HelloGridView.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace HelloGridView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class HelloGridView : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var gridView = FindViewById<GridView>(Resource.Id.gridView1);
+            gridView.Adapter = new ImageAdapter(this);
+
+            gridView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+            {
+                Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
+            };
+        }
+    }
+}
---
+++ HelloGridView/HelloGridView/ImageAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloGridView
+{
+    class ImageAdapter : BaseAdapter
+    {
+        Context context;
+
+        public ImageAdapter(Context c)
+        {
+            context = c;
+        }
+
+        public override int Count
+        {
+            get { return thumbIds.Length; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+            if(convertView == null)
+            {
+                imageView = new ImageView(context);
+                imageView.LayoutParameters = new GridView.LayoutParams(85, 85);
+                imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+                imageView.SetPadding(8, 8, 8, 8);
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+
+            imageView.SetImageResource(thumbIds[position]);
+            return imageView;
+        }
+
+        int[] thumbIds =
+        {
+            Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+            Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+            Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+            Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+            Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+            Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+            Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+            Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+            Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+            Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+            Resource.Drawable.sample_6, Resource.Drawable.sample_7
+        };
+    }
+}
